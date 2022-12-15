ec19d1aaa142e17f4b875d36a39411e33eda4404
Soham <xzoham@gmail.com>
2022-12-15 15:26:20 -0500

    fix

--- src/components/image-gallery/ImageGallery.js
+++ src/components/image-gallery/ImageGallery.js
@@ -39,16 +39,17 @@ const ImageGallery = () => {
             break
         case 'Film Spotlight':
             info = 'Looking for something amazing to watch but you feel like you’ve seen everything? Oh we got something for you. '
-            title = 'Pulp Fiction'
-            image = FilmImage
+            title = 'Kill Bill (Vol 1)'
             Ilink = 'https://www.imdb.com/title/tt0110912/?ref_=ext_shr_lnk'
+            video = "https://www.youtube.com/embed/mLFf1A2K4MQ?autoplay=1"
+            text = "If you can’t stomach brutal action scenes, just look away now. The first of a two-part saga, written and directed by the master Quentin Tarantino, Kill Bill is not only packed with some of the most beautifully choreographed fight scenes ever, but everything about it is top tier. The story, the music, the hilariously clever dialogue, all is Tarantino at his best. If you haven’t watched it, watch it."
             break
         case 'Song of the Day':
             info = 'QUICK, need something to listen to? EVERY morning, have a song of the day waiting for you.'
             title = 'Umi Says by Mos Def'
             Slink = 'https://open.spotify.com/track/6FBzhcfgGacfXF3AmtfEaX?si=c50892d435694499'
             Alink = 'https://music.apple.com/us/album/c-u-girl/1030015256?i=1030015261'
-            video = '"https://www.youtube.com/embed/vntLKOd9saI"'
+            video = 'https://www.youtube.com/embed/vntLKOd9saI?autoplay=1'
             text = "UMI = meaning “Mother” in Arabic. On the surface, UMI Says is soft and beautiful, thanks to its super smooth jazzy production, with assistance from a young will.i.am on the keys. Despite its soft tone, it serves as a powerful calling for the freedom of black people, recalling the everyday struggle that is living with dark colored skin, but reminding the relentless power we have within us to shine through it. “Shine your light on the world, shine your light for the world to see.” "
             break
         default:
@@ -65,7 +66,7 @@ const ImageGallery = () => {
                         {info && <p className='modal__info'>{info}</p>}
                         {title && <h4 className='modal__title'>{title}</h4>}
                         {image && <img src={image} className='modal__image' />}
-                        {video && <div className='video-player'> <iframe width="560" height="315" src="https://www.youtube.com/embed/vntLKOd9saI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> </div>}
+                        {video && <div className='video-player'> <iframe width="560" height="315" src={video} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> </div>}
                         {text && <p className="modal__info">{text}</p>}
                         {(Slink || Alink) && <h3 style={{ padding: '2rem' }}>Stream it on</h3>}
                         {(Ilink) && <h3 style={{ padding: '3rem' }}>Learn more at</h3>}
@@ -78,6 +79,9 @@ const ImageGallery = () => {
             )
             }
             <div className='image-gallery'>
+                <button onClick={() => setSpotlight('Song of the Day')}>
+                    <div className='single-spotlight'><div className='single-spotlight__text'><h4 className='single-spotlight__text'>Song of the Day</h4></div> </div>
+                </button>
                 <button onClick={() => setSpotlight('Did you know')}>
                     <div className='single-spotlight '><div className='single-spotlight__text'><h4>Did you know?</h4></div></div>
                 </button>
@@ -89,9 +93,6 @@ const ImageGallery = () => {
                 </button>
                 <button onClick={() => setSpotlight('Film Spotlight')}>
                     <div className='single-spotlight'><div className='single-spotlight__text'><h4 className='single-spotlight__text'>Film Spotlight</h4></div> </div>
-                </button>
-                <button onClick={() => setSpotlight('Song of the Day')}>
-                    <div className='single-spotlight'><div className='single-spotlight__text'><h4 className='single-spotlight__text'>Song of the Day</h4></div> </div>
                 </button>
             </div>
         </div >
