00a7375fe4d6e2893c2703abca8474c80b0ce943
Soham <xzoham@gmail.com>
2022-12-15 20:17:15 -0500

    s

--- src/App.js
+++ src/App.js
@@ -1,37 +1,44 @@
 import Layout from './components/Layout';
 import React, { useEffect, useRef, useState, useMemo } from 'react';
 import './scss/main.scss'
-import { GraphQLClient } from 'graphql-request';
 import SOSlogo from './images/logo/SOSlogo.png'
 import Needle from './images/needle.png'
 function App() {
   const [showStarting, setStarting] = useState(true)
   const [showRotation, setRotation] = useState(false)
-
-  const hygraph = new GraphQLClient(
-    'https://api-us-east-1.hygraph.com/v2/clbn867yy0xc001uoa0m0csyb/master'
-  );
-  const delay = ms => new Promise(
-    resolve => setTimeout(resolve, ms)
-  );
   const handleClick = async event => {
     setRotation(true)
     await delay(2000);
     setStarting(false)
+
   }
   return (
     <div >
       {showStarting &&
         <div>
-          <div className={`start-logo ${showRotation && 'animate-rot'}`}>
-            <img src={SOSlogo} className='main-logo' />
-            <div className='play-container'>
-              <button onClick={() => handleClick()}>
-                <a className='play-btn' />
-                <h1>Press Play</h1>
-              </button>
-            </div>
+          <div class="loader">
+            <div class="bar1"></div>
+            <div class="bar2"></div>
+            <div class="bar3"></div>
+            <div class="bar4"></div>
+            <div class="bar5"></div>
+            <div class="bar6"></div>
           </div>
+          <main>
+            <div className='-content -index'>
+              <div className={`start-logo ${showRotation && 'animate-rot'}`}>
+                <div className='start-main-container'>
+                  <img src={SOSlogo} className='main-logo' />
+                </div>
+                <div className='play-container'>
+                  <button onClick={() => handleClick()}>
+                    <a className='play-btn js-trigger-transition' />
+                    <h1>Press Play</h1>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </main>
         </div>
       }
       {!showStarting &&
