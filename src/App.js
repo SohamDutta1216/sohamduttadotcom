45ea249c2057ad5ae839feb1b7efb7ab3a33b7da
Soham <xzoham@gmail.com>
2022-12-15 22:12:44 -0500

    n

--- src/App.js
+++ src/App.js
@@ -3,45 +3,36 @@ import React, { useEffect, useRef, useState, useMemo } from 'react';
 import './scss/main.scss'
 import SOSlogo from './images/logo/SOSlogo.png'
 import Needle from './images/needle.png'
+import Black from './images/black.png'
 function App() {
   const [showStarting, setStarting] = useState(true)
   const [showRotation, setRotation] = useState(false)
+  const [showTransition, setTransition] = useState(false)
   const delay = ms => new Promise(
     resolve => setTimeout(resolve, ms)
   );
   const handleClick = async event => {
     setRotation(true)
-    await delay(2000);
+    setTransition(true)
+    await delay(3000);
     setStarting(false)
-
   }
   return (
     <div >
       {showStarting &&
         <div>
-          <div class="loader">
-            <div class="bar1"></div>
-            <div class="bar2"></div>
-            <div class="bar3"></div>
-            <div class="bar4"></div>
-            <div class="bar5"></div>
-            <div class="bar6"></div>
-          </div>
-          <main>
-            <div className='-content -index'>
-              <div className={`start-logo ${showRotation && 'animate-rot'}`}>
-                <div className='start-main-container'>
-                  <img src={SOSlogo} className='main-logo' />
-                </div>
-                <div className='play-container'>
-                  <button onClick={() => handleClick()}>
-                    <a className='play-btn js-trigger-transition' />
-                    <h1>Press Play</h1>
-                  </button>
-                </div>
-              </div>
+          <img src={Black} style={{ display: 'none' }} className={`${showTransition && 'black'}`} />
+          <div className={`start-logo ${showRotation && 'animate-rot'}`}>
+            <div className='start-main-container'>
+              <img src={SOSlogo} className='main-logo' />
             </div>
-          </main>
+            <div className='play-container'>
+              <button onClick={() => handleClick()}>
+                <a className='play-btn js-trigger-transition' />
+                <h1>Press Play</h1>
+              </button>
+            </div>
+          </div>
         </div>
       }
       {!showStarting &&
