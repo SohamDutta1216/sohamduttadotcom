c69f53fa7b6ed36ab48555a914365f9930c56527
Soham <xzoham@gmail.com>
2023-02-03 11:33:44 -0500

    f

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -19,7 +19,7 @@ const About = (isInView) => {
             <div className='up'>
                 {showButton &&
                     <>
-                        <a href="#home" class="to-top">Back to top</a>
+                        <a href="#home" class="to-top">back to top</a>
                         <div className='social-wrapper-mobile'><a href="https://www.instagram.com/soundsofspotlight/" target="_blank" ><Instagram /></a><a href="https://www.tiktok.com/@soundsofspotlight" target="_blank" ><Tiktok /></a></div>
                         <div className='social-wrapper-tablet'><a href="https://www.instagram.com/soundsofspotlight/" target="_blank" ><Instagram /></a><a href="https://www.tiktok.com/@soundsofspotlight" target="_blank" ><Tiktok /></a></div>
                     </>
@@ -30,7 +30,10 @@ const About = (isInView) => {
                     <div className=' about__row'>
                         <div className=' about__column'>
                             <h1 className='neonText about__header'>WHO ARE WE?</h1>
-                            <p className="about__text">We are Sounds of Spotlight, a movie and music enthusiasts favorite site. Bringing curated entertainment to you daily that you can't find anywhere else. </p>
+                            <p className="about__text">
+                                In summary, Sounds of Spotlight is a place to not just find, but really learn about songs, artists, genres, movies, from all different genres and from all over the world, especially ones that deserve more love and recognition.<br /><br /> As someone who's really passionate about all types of music, film, etc., and loves talking about them, I really wanted to find a creative community where anyone wanting that outlet can connect and share that passion or just sit and enjoy, both online and at our in-person events. More details coming soon!
+
+                            </p>
                         </div>
                         <div className='logo-container'>
                             <img src={SOSlogo} className='about__logo' />
