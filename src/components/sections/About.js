d3d7908af0df3052c898f5f957c3a5a9e25e4371
Soham <xzoham@gmail.com>
2023-02-03 16:31:01 -0500

    s

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -31,7 +31,7 @@ const About = (isInView) => {
                         <div className=' about__column'>
                             <h1 className='neonText about__header'>WHO ARE WE?</h1>
                             <p className="about__text">
-                                In summary, Sounds of Spotlight is a place to not just find, but really learn about songs, artists, genres, movies, from all different genres and from all over the world, especially ones that deserve more love and recognition.<br /><br /> As someone who's really passionate about all types of music, film, etc., and loves talking about them, I really wanted to find a creative community where anyone wanting that outlet can connect and share that passion or just sit and enjoy, both online and at our in-person events. More details coming soon!
+                                In summary, Sounds of Spotlight is a place to not just find, but really learn and talk about songs, artists, movies, etc. from all different genres and from all over the world, especially ones that deserve more love and recognition.<br /><br /> As someone who's really passionate about all types of music, film, etc., and loves talking about them, I really wanted to find a creative community where anyone wanting that outlet can connect and share that passion or just sit and enjoy, both online and at our in-person events. More details coming soon!
 
                             </p>
                         </div>
