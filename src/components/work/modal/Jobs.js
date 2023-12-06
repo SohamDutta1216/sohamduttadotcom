ebb544cd8d6b9344a02b0b18ea62358116f44a98
Soham <xzoham@gmail.com>
2023-12-06 12:52:36 -0500

    fix: fixed nav icon size

--- src/components/work/modal/Jobs.js
+++ src/components/work/modal/Jobs.js
@@ -19,9 +19,9 @@ const Jobs = () => {
                     <p>Front End Developer</p>
                     <p>Feb 2022 - Dec 2023 · 1 yr 11 months · Full-Time</p>
                     <p>Remote</p>
-                    <p>- helped migrate existing CMS from Kentico to Strapi whilst optimizing and
+                    <p>- Helped migrate existing CMS from Kentico to Strapi whilst optimizing and
                         updating all components within our software.</p>
-                    <p>Performed debugging and troubleshooting of applications using JIRA.</p>
+                    <p>- Performed debugging and troubleshooting of applications using JIRA.</p>
                     <p>- Wrote detailed technical documentation outlining system architecture,
                         coding conventions and best practices.</p>
                     <p>- Implemented agile development practices across multiple projects,
@@ -41,23 +41,25 @@ const Jobs = () => {
                     <p>Front End Developer</p>
                     <p>May 2021 - Feb 2022 · 10 mos · Full-Time</p>
                     <p>Remote</p>
-                    <p>- Transferred over legacy code base to a modern framework using React,
-                        Gatsby, and Kentico as one of the first developers on the team with React
-                        and Javascript experience.</p>
-                    <p>- Developed a scalable templating software for our companies clients to
-                        quickly deploy fully customizable websites.</p>
-                    <p>- Executed software build and release processes.</p>
-                    <p>- Wrote technical documentation for users and colleagues regarding
-                        system architecture and development process.</p>
-                    <p>- Researched and implemented emerging technologies and best practices
-                        in software engineering.</p>
-                    <p> Analyzed user needs to determine necessary improvements to existing
-                        software.</p>
-                    <p>- Participated in Agile sprint planning meetings to discuss development
-                        tasks.</p>
-                    <p>- Debugged issues in existing codebase to identify root cause of problems.</p>
-                    <p>- Integrated third-party APIs into existing applications for additional
-                        functionality.</p>
+                    <div>
+                        <p>- Transferred over legacy code base to a modern framework using React,
+                            Gatsby, and Kentico as one of the first developers on the team with React
+                            and Javascript experience.</p>
+                        <p>- Developed a scalable templating software for our companies clients to
+                            quickly deploy fully customizable websites.</p>
+                        <p>- Executed software build and release processes.</p>
+                        <p>- Wrote technical documentation for users and colleagues regarding
+                            system architecture and development process.</p>
+                        <p>- Researched and implemented emerging technologies and best practices
+                            in software engineering.</p>
+                        <p>- Analyzed user needs to determine necessary improvements to existing
+                            software.</p>
+                        <p>- Participated in Agile sprint planning meetings to discuss development
+                            tasks.</p>
+                        <p>- Debugged issues in existing codebase to identify root cause of problems.</p>
+                        <p>- Integrated third-party APIs into existing applications for additional
+                            functionality.</p>
+                    </div>
                     <h4>Skills:</h4>
                     <p> GatsbyJS · Kentico · Agile Methodologies · TypeScript · SCSS · SASS · Content Management Systems (CMS) · Adobe XD · GraphQL · JavaScript · HTML5 · Node.js · React.js · React Hooks</p>
 
