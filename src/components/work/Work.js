96a6549f64407edc50cc6dd6714242ac97993860
Soham <xzoham@gmail.com>
2023-12-06 12:11:01 -0500

    fix: mobile view fix

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -8,6 +8,8 @@ import Projects from '../../images/icons/projects.png'
 import Home from '../../images/icons/home.png'
 import { Link } from 'react-router-dom';
 import Modal from './modal/Modal';
+import Div100vh from 'react-div-100vh';
+
 const Work = () => {
     var [date, setDate] = useState(new Date());
     useEffect(() => {
@@ -23,44 +25,46 @@ const Work = () => {
     const [size, setSize] = useState(false)
     return (
         <div>
-            <img className="vintage-bg" src={VintageBG}></img>
-            <section className="screen-change">
-                <p className='work'>
-                    <div className='work-column'>
-                        <div className='windows-screen'>
-                            <div className='windows-column'>
-                                <Link to='/home'><div className='windows-icon'>
-                                    <img src={Home} />
-                                    <p>Home</p>
-                                </div></Link>
-                                <div onClick={() => setClick('Jobs')} className='windows-icon'>
-                                    <img src={Jobs} />
-                                    <p>Work Experience</p>
+            <Div100vh>
+                <img className="vintage-bg" src={VintageBG}></img>
+                <section className="screen-change">
+                    <p className='work'>
+                        <div className='work-column'>
+                            <div className='windows-screen'>
+                                <div className='windows-column'>
+                                    <Link to='/home'><div className='windows-icon'>
+                                        <img src={Home} />
+                                        <p>Home</p>
+                                    </div></Link>
+                                    <div onClick={() => setClick('Jobs')} className='windows-icon'>
+                                        <img src={Jobs} />
+                                        <p>Work Experience</p>
+                                    </div>
+                                    <a className='windows-icon'>
+                                        <img src={Resume} />
+                                        <p>Resume</p>
+                                    </a>
+                                    <div onClick={() => setClick('Projects')} className='windows-icon'>
+                                        <img src={Projects} />
+                                        <p>Projects</p>
+                                    </div>
                                 </div>
-                                <a className='windows-icon'>
-                                    <img src={Resume} />
-                                    <p>Resume</p>
-                                </a>
-                                <div onClick={() => setClick('Projects')} className='windows-icon'>
-                                    <img src={Projects} />
-                                    <p>Projects</p>
-                                </div>
-                            </div>
-                            {click !== '' &&
-                                <Modal setClick={setClick} click={click} setSize={setSize} size={size} />
-                            }
-
-                        </div>
-                        <div className='work-bar'>
-                            <div className='work-start'>
-                                <button className='windows-logo'><img src={Windows} /><p>Start</p></button>
-                                <div className='windows-time'>{formattedTime}</div>
+                                {click !== '' &&
+                                    <Modal setClick={setClick} click={click} setSize={setSize} size={size} />
+                                }
 
                             </div>
+                            <div className='work-bar'>
+                                <div className='work-start'>
+                                    <button className='windows-logo'><img src={Windows} /><p>Start</p></button>
+                                    <div className='windows-time'>{formattedTime}</div>
+
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </p>
-            </section>
+                    </p>
+                </section>
+            </Div100vh>
         </div >
 
     );
